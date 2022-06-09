c939684313b42f767e1cd50432a3503034ae4962
Rafael Andres Daza <radazn1998@gmail.com>
2022-06-09 17:17:11 -0500

    Implementación de algoritmo Fibonacci y ejecución de casos de prueba

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,2 +1,5 @@
-def CalcularSerie():
-    return None
+def CalcularSerie(n):
+    serie = [0, 1]
+    for i in range(2, n):
+        serie.append(serie[i-1]+serie[i-2])
+    return serie
--- Test.py
+++ Test.py
@@ -1,5 +1,11 @@
 import Fibonacci
 
 
-def test():
-    assert Fibonacci.CalcularSerie() == True
+def testFibonacci(n=6):
+    serie = [0, 1, 2, 3, 4, 5]
+    assert Fibonacci.CalcularSerie(n) == serie
+
+
+def testFibonacci2(n=6):
+    serie = [0, 1, 1, 2, 3, 5]
+    assert Fibonacci.CalcularSerie(n) == serie
